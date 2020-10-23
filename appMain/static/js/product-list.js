5599fb7b9209d4ff5886023b53d7bc7d72e73a9e
김승규 <airaen0129@gmail.com>
2020-10-23 22:49:10 +0900

    동적으로 페이지 변경 및 데이터 전달
    ajax: 동적으로 변경
    requests: api 호출
    form(get, post): 페이지간 데이터 전달

--- appMain/static/js/product-list.js
+++ appMain/static/js/product-list.js
@@ -1,30 +1,47 @@
-let selectStoreName = '롭스 건대점'
+let selectStoreName = "롭스 병천점";
+console.log(csrf_token);
 
-fetch('http://127.0.0.1:8000/api/store?name=' + selectStoreName)
-    .then(res => res.json())
-    .then(res => {
-        // data를 응답 받은 후의 로직
-        const selectStoreID = res[0]['id'];
-        
-        fetch('http://127.0.0.1:8000/api/sell?store=' + selectStoreID)
-            .then(res => res.json())
-            .then(res => {
-                const sellProductID = res[0]['id'];
-                
-                
-                $("#product-list").append(`
-                <div class="col-sm-6 item">
-                    <div class="row">
-                        <div class="col-md-12 col-lg-5">
-                                <img class="img-fluid" src="` + noImage + `">
-                        </div>
-                        <div class="col">
-                            <p class="description" style="font-size: 16px;"><strong style="margin-right: 5px;">{{ p.brand }}</strong>{{ p.name }}</p>\
-                            <span style="font-size: 20px;"><strong>스트롱</strong></span><span>원</span>\
-                        </div>
-                        <a class="stretched-link" href="{% url 'detail' %}?productID={{p.id}}&storeID=1"></a>
-                    </div>
+$.ajax({
+    url: home,
+    method: "POST",          // HTTP 요청 방식(GET, POST)
+    dataType: "json",           // 서버에서 보내줄 데이터의 타입(default값으로 json으로 되어있다.)
+    data: {'storeName':selectStoreName, 'csrfmiddlewaretoken': csrf_token}   // HTTP 요청과 함께 서버로 보낼 데이터
+}).done(data => {    // HTTP 요청 성공 시, 요청한 데이터가 done() 메소드로 전달
+    //alert('성공!')
+    //console.log(data.products)
+    $("#product-list").empty();
+    data.products.forEach((product, idx) => {
+        //console.log(product)
+        let mainImage;
+        if (!product.mainImage) mainImage = noImage;
+        else mainImage = product.mainImage;
+
+        let html = `
+        <div class="col-sm-6 item">
+            <div class="row">
+                <div class="col-md-12 col-lg-5">
+                    <img class="img-fluid" src="`+mainImage+`">
                 </div>
-                `).trigger("create");
-        });
+            <div class="col">
+                <p class="description" style="font-size: 16px;"><strong style="margin-right: 5px;">`+product.brand+`</strong>`+product.name+`</p>
+                <span style="font-size: 20px;"><strong>`+product.price+`</strong></span><span>원</span>
+            </div>
+            <form action="`+detail+`" method="get">
+            `
+        // <a class="stretched-link" href="`+detail+`?product=`+product.id+`&store=`+product.store+`"></a>
+        for (const key in product) {
+            html += `<div class="d-none"><label>`+key+`</label><input name="`+key+`" value="`+product[key]+`" /></div>`;
+        }
+
+        html += `<button class="stretched-link" type="submit" style="opacity: 0;"></form>`
+
+        $("#product-list").append(html);
+
+        
     });
+
+}).fail((xhr, data) => {     // HTTP 요청 실패 시, 오류와 상태에 관한 정보가 fail() 메소드로 전달
+    alert('실패!')
+}).always((xhr, data) => {   // HTTP 요청의 성공여부와는 상관없이 언제나 always()메소드 실행
+    //alert('항상!')
+})
--- appMain/views.py
+++ appMain/views.py
@@ -1,21 +1,58 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 import requests
+from django.http.response import HttpResponse
+
+from LotteIsHorse.settings import BASE_URL  # url 주소
 
 # Create your views here.
 def home(request):
-    product = requests.get('http://localhost:8000/api/product', auth=('admin', 'admin'))
-    return render(request, 'home.html', {'product' : product.json()})
+    if request.method == "POST":
+        try:
+            if (request.POST.get('storeName') == ""):
+                raise
+            store = requests.get(BASE_URL + 'store?name=' + request.POST.get('storeName'), auth=('admin', 'admin')).json()
+            sells = requests.get(BASE_URL + 'sell?store=' + str(store[0]['id']), auth=('admin', 'admin')).json()
+            products = []
+            for sell in sells:
+                products.extend(requests.get(BASE_URL + 'product?id=' + str(sell['product']), auth=('admin', 'admin')).json())
+
+            # 하나로 합치기
+            for product in products:
+                product['store'] = store[0]['id']
+                product['storeName'] = store[0]['name']
+                for sell in sells:
+                    if (product['id'] == sell['product']):
+                        product['sell'] = sell['id']
+                        break
+
+            responseDic = {
+                'products': products
+            }
+        
+            # 출력
+            # for product in products:
+            #     print("\n===========" + product['name'])
+            #     for i in product:
+            #         print(str(i) + " : " + str(product[i]))
+
+            return JsonResponse(responseDic)
+        except:
+            return
+    else:
+        return render(request, 'home.html')
 
 def detail(request):
-    requestGet = request.GET
-    store = requests.get('http://localhost:8000/api/store?id=' + requestGet.get('store'), auth=('admin', 'admin'))
-    product = requests.get('http://localhost:8000/api/product?id=' + requestGet.get('product'), auth=('admin', 'admin'))
-    sell = requests.get('http://localhost:8000/api/sell?store=' + requestGet.get('store') + '&product=' + requestGet.get('product'), auth=('admin', 'admin'))
-    
-    returnDic = {
-        'store': store.json(),
-        'product': product.json(),
-        'sell': sell.json(),
-    }
+    responseDic = {}
+    for key, value in request.GET.items():
+        responseDic[key] = value
 
-    return render(request, 'detail.html', returnDic)
+    sell = requests.get(BASE_URL + 'sell?id=' + responseDic['sell'], auth=('admin', 'admin')).json()
+
+    responseDic['count'] = sell[0]['count']
+
+    # 출력
+    # for key, value in responseDic.items():
+    #     print(str(key) + " : " + str(value))
+    
+    return render(request, 'detail.html', responseDic)
