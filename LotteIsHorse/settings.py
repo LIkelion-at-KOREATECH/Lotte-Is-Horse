ab8c67a978864ea768e38c90c27365a65356a6af
DESKTOP-GK1C49Q\Cloud <gomdalp1701@gmail.com>
2020-10-26 06:41:14 +0900

    requirements.txt업데이트

--- LotteIsHorse/settings.py
+++ LotteIsHorse/settings.py
@@ -21,9 +21,11 @@ BASE_URL = "http://3.35.243.185:8000/api/"
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '0!v-@l4^s8ix-z5tsr$wl3kk949at9i%+)umde6tz@a!(f3p)6'
-
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
+# DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
